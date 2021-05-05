c14709323f5171ab5385b62622727e40092bd5bd
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-05 13:08:00 +0200

    Var and Function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 /*
-Consegna
 Il computer deve generare 16 numeri casuali tra 1 e 100.
 I numeri non possono essere duplicati.
 In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
@@ -8,10 +7,38 @@ Se il numero è presente nella lista dei numeri generati, la partita termina, al
 La partita termina quando il giocatore inserisce un numero "vietato" o raggiunge il numero massimo possibile di numeri consentiti.
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha inserito un numero consentito.
 
-BONUS: (da fare solo se funziona tutto il resto)
 all'inizio il software richiede anche una difficoltà all'utente che cambia il range di numeri casuali:
 con difficoltà 0 => tra 1 e 100
 con difficoltà 1 => tra 1 e 80
 con difficoltà 2 => tra 1 e 50
 */
 
+// -------------------------VARIABILI-------------------------
+var bombs = [];
+var attempts = [];
+var maxAttempts = 84;
+// -------------------------/VARIABILI------------------------
+
+// -------------------------FUNZIONI--------------------------
+function randomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function isInArray (number, array) {
+
+    for (i = 0; i < array.length; i++) {
+        if (number == array[i]) {
+            return true;
+        }
+    }
+    return false;
+
+}
+// -------------------------/FUNZIONI-------------------------
+
+// CREA BOMBE
+
+// /CREA BOMBE
+
+// GIOCO
+// /GIOCO
